bdcd06a188c8154471153c4c4f560c614555b43a
Artem Bazyl <a4b11s@gmail.com>
2023-09-19 20:48:17 +0300

    Feature/comand line attributes (#3)
    * feature: add agrparser_init.py
    
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>
    
    * feature: add -e param
    
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>
    
    * feature: add args_params
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>
    
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>
    
    * feature: add args_params
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>
    
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>
    
    ---------
    
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>

---
+++ configuration/agrparser_init.py
@@ -0,0 +1,43 @@
+import argparse
+
+
+def agrparser_init():
+    parser = argparse.ArgumentParser(
+        prog='SA_WG_GAN',
+        description='Self-attention WGAN'
+    )
+
+    parser.add_argument(
+        '-hp',
+        '--history_plot',
+        action='store',
+        type=str,
+        dest="logfile_path",
+        help="Plot training history from log file"
+    )
+
+    parser.add_argument(
+        '-t',
+        '--train',
+        action='store_true',
+        help="Start training"
+    )
+
+    parser.add_argument(
+        '-e',
+        '--epoch',
+        action='store',
+        type=int,
+        dest="epoch_count",
+        help="Start training",
+        default=20
+    )
+
+    parser.add_argument(
+        '-sa',
+        '--show_activation',
+        action='store_true',
+        help="Show layers activation"
+    )
+
+    return parser
