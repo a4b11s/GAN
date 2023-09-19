3d3abcb02d5c8ee144de2f30c7cc91e27dde63e4
Artem Bazyl <a4b11s@gmail.com>
2023-09-19 03:37:01 +0300

    feature: add calling activations_show.py and plot_history in main
    Signed-off-by: Artem Bazyl <a4b11s@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from activations_show import activations_show
+from plot_history import plot_history
 from train import start_train
 
 if __name__ == "__main__":
     start_train()
+    plot_history()
+    activations_show()
